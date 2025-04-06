9af4dd9fcf660cc2c0510bbbfaffbec9b2302305
bryanolmstead-vr <bryan.olmstead@visualrobotics.com>
2025-04-05 23:38:16 -0700

    progress

--- mrp/src/main/java/com/bushnell/StockReport.java
+++ mrp/src/main/java/com/bushnell/StockReport.java
@@ -21,8 +21,11 @@ import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 //import javax.swing.plaf.metal.MetalCheckBoxIcon;
+import javax.swing.ScrollPaneConstants;
 
 import com.bushnell.GUI;
 import com.bushnell.Database;
@@ -48,13 +51,25 @@ public class StockReport {
         GUI.setDimension(titleBox, 600, 100);
         panel.add(titleBox);
 
+        // create text pane
+        Box textBox = Box.createHorizontalBox();
+        textBox.add(Box.createRigidArea(new Dimension(20,0)));
+        JTextArea stockText = new JTextArea(30, 60);  // 30 rows and 60 columns
+        stockText.setEditable(false); 
+        JScrollPane scroll = new JScrollPane(stockText);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
         List<Part> allSkuList = Database.getAllSkuData();
-        Integer i=1;
+        //Integer i=1;
         for( Part part : allSkuList) {
-            System.out.println("i=" + Integer.toString(i) + " sku=" + part.sku + " description=\"" + part.description + "\" " +
-                               "price=" + Double.toString(part.price) + " stock=" + Integer.toString(part.stock));
-            i++;
+            //System.out.println("i=" + Integer.toString(i) + " sku=" + part.sku + " description=\"" + part.description + "\" " +
+            //                   "price=" + Double.toString(part.price) + " stock=" + Integer.toString(part.stock));
+            stockText.append(String.format("%20s %4.2f %4d %s\n", part.sku, part.price, part.stock, part.description));
+            //i++;
         }
+        textBox.add(scroll);
+        textBox.add(Box.createRigidArea(new Dimension(20,0)));
+        panel.add(textBox);
 
         return panel; 
     }
